cd84b332a021b2395e3435917bd55f9fc828de38
David <d888eightsix@icloud.com>
2023-11-14 15:38:46 +0700

    7 comm

--- components/Authen.tsx
+++ components/Authen.tsx
@@ -81,25 +81,17 @@ function Authentication() {
           <>
             <div className="auth__content">
               <div className="auth__title">Log in to Summarist</div>
-              <div className="auth__error">{error}</div>
+              <div className="auth__error">{error ? `Firebase: ${error}` : null}</div>
               <button
                 className="btn guest__btn--wrapper"
                 onClick={() => {
                   guestSignInHandler();
                 }}
               >
-                {/* <div className="guest__icon--wrapper facebook__icon--wrapper">
-                  <BsFillPersonFill className="guest__icon" />
-                </div> */}
 
 <figure className="google__icon--mask guest__icon--mask">
 <BiSolidUser className="guest__icon" />
 </figure>
-
-
-{/* <figure className="google__icon--mask guest__icon--mask">
-  <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 448 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M224 256c70.7 0 128-57.3 128-128S294.7 0 224 0 96 57.3 96 128s57.3 128 128 128zm89.6 32h-16.7c-22.2 10.2-46.9 16-72.9 16s-50.6-5.8-72.9-16h-16.7C60.2 288 0 348.2 0 422.4V464c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48v-41.6c0-74.2-60.2-134.4-134.4-134.4z"></path></svg>
-</figure> */}
 
                 Login as a Guest
               </button>
@@ -175,7 +167,7 @@ function Authentication() {
           <>
             <div className="auth__content">
               <div className="auth__title">Sign up to Summarist</div>
-              <div className="auth__error">{error}</div>
+              
               <button className="btn google__btn--wrapper">
                 <div className="guest__icon--wrapper">
                   <FcGoogle className="google__icon" />
@@ -186,6 +178,8 @@ function Authentication() {
               <div className="auth__separator">
                 <span className="auth__separator--text">or</span>
               </div>
+
+              <div className="auth__error">{error ? `Firebase: ${error}` : null}</div>
 
               <form
                 action=""
