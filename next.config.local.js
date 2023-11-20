cec36e9d55ae3feb1be01c51a6e9b34140d33a95
David <145085068+david688front@users.noreply.github.com>
2023-11-20 14:39:45 +0700

    Update next.config.local.js

--- next.config.local.js
+++ next.config.local.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    //reactStrictMode: true,
-    env: {
-      STRIPE_SECRET_KEY: 'sk_test_51OAQ5hL3S5nVxXEOwMAsWtWywSFcFDCPOYT7aBBzXAYpcPwDTSMEJxedax67fOphh5AToOLZ7EMZ0L9lATElNx9700t5iuOJAD',
-      STRIPE_PUBLISHABLE_KEY: 'pk_test_51OAQ5hL3S5nVxXEO0ROqCjGBKFEE5mKKhbzPF0QA28EJ03TMTlp1BZ83VF89GCg1oDU3nI2UoyVt6SrkedOZeshu009O5mlIPi'
-    },
+    reactStrictMode: true
    }
   
   module.exports = nextConfig
