e3f7ed1057af18df6949776316840c93ca6a78f4
David <d888eightsix@icloud.com>
2023-11-20 11:50:14 +0700

    8.2

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,10 @@
 import Authen from "@/components/Authen";
-import Features from "@/components/home/features";
-import Footer from "@/components/home/footer";
-import Header from "@/components/home/header";
-import Landing from "@/components/home/landing";
-import Numbers from "@/components/home/numbers";
-import Reviews from "@/components/home/reviews";
+import Features from "@/components/home/Features";
+import Footer from "@/components/home/Footer";
+import Header from "@/components/home/Header";
+import Landing from "@/components/home/Landing";
+import Numbers from "@/components/home/Numbers";
+import Reviews from "@/components/home/Reviews";
 import { RootState } from "@/redux/modalStore";
 import Head from "next/head";
 import { useSelector } from "react-redux";
